3a342840d78b1d66149449a2616eb36dca47d689
Anwesh43 <anweshthecool0@gmail.com>
2017-01-22 09:03:13 +0530

    Adding activity files

---
+++ app/src/main/java/com/anwesome/game/trispy/GameAcivity.java
@@ -0,0 +1,14 @@
+package com.anwesome.game.trispy;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by anweshmishra on 22/01/17.
+ */
+public class GameAcivity extends AppCompatActivity{
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/anwesome/game/trispy/MainActivity.java
@@ -0,0 +1,26 @@
+package com.anwesome.game.trispy;
+
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.anwesome.game.trispy.views.GameView;
+
+public class MainActivity extends AppCompatActivity {
+    private GameView gameView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        gameView = new GameView(this);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(gameView);
+    }
+    public void onPause() {
+        super.onPause();
+        gameView.pause();
+    }
+    public void onResume() {
+        super.onResume();
+        gameView.resume();
+    }
+}
