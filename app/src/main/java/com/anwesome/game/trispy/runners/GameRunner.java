917115c62c784bd729f2d0ddefabe84e1ae9ae27
Anwesh43 <anweshthecool0@gmail.com>
2017-01-22 14:15:33 +0530

    Adding logic for ball movement and catching ball and scoring logic

--- app/src/main/java/com/anwesome/game/trispy/runners/GameRunner.java
+++ app/src/main/java/com/anwesome/game/trispy/runners/GameRunner.java
@@ -3,6 +3,7 @@ package com.anwesome.game.trispy.runners;
 import android.graphics.*;
 import android.view.*;
 import com.anwesome.game.trispy.GameConstants;
+import com.anwesome.game.trispy.gameobjects.MovingBall;
 import com.anwesome.game.trispy.gameobjects.Ring;
 import com.anwesome.game.trispy.gameobjects.RotatingLine;
 import com.anwesome.game.trispy.utils.GameCreateUtil;
@@ -17,9 +18,12 @@ public class GameRunner implements Runnable{
     private int time = 0;
     private int level = 1;
     private int w,h;
+    private int currentRingColor = 0;
     private ConcurrentLinkedQueue<Ring> rings = new ConcurrentLinkedQueue<>();
+    private ConcurrentLinkedQueue<MovingBall> balls = new ConcurrentLinkedQueue<>();
     private RotatingLine rotatingLine;
     private SurfaceHolder surfaceHolder;
+    private MovingBall currentBall;
     private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     public void run() {
         while(isRunning) {
@@ -30,26 +34,66 @@ public class GameRunner implements Runnable{
                     h = canvas.getHeight();
                     rings = GameCreateUtil.createRings();
                     rotatingLine = RotatingLine.newInstance();
+                    currentBall = GameCreateUtil.createBall(0,w);
+                    balls.add(currentBall);
                 }
                 canvas.drawColor(GameConstants.BACK_COLOR);
                 rotatingLine.draw(canvas,paint);
+                for(MovingBall movingBall:balls) {
+                    movingBall.setRotSpeed(rotatingLine.getSpeed());
+                    movingBall.draw(canvas,paint);
+
+                }
                 for(Ring ring:rings) {
                     ring.draw(canvas,paint);
                     if(rotatingLine.getRot() == ring.getDeg()) {
                         rotatingLine.setSpeed(0);
+                        float currentBallDeg = currentBall.getDeg();
+                        int ringColorIndexForCurrentBall = (int)(currentBallDeg/90);
+                        currentRingColor = checkForRingColor(ringColorIndexForCurrentBall);
                     }
                 }
+                if(currentBall!=null && currentBall.isAtEdge()) {
+                    if (currentRingColor == currentBall.getColor()) {
+                        balls.remove(currentBall);
+                        //Code to set first ball of the collection as currentBall
+                        if(balls.size()>0) {
+                            for(MovingBall ball:balls) {
+                                currentBall = ball;
+                                break;
+                            }
+                        }
+                        else {
+                            currentBall = null;
+                        }
+                    }
+
+                    else {
+                        isRunning = false;
+                    }
+                }
+                currentRingColor = 0;
                 surfaceHolder.unlockCanvasAndPost(canvas);
                 time++;
+                GameCreateUtil.createMovingBallForLevel(time,level,w,rotatingLine,balls);
             }
             try {
-
                 Thread.sleep(GameConstants.GAME_DELAY);
             }
             catch(Exception ex) {
 
             }
         }
+    }
+    private int checkForRingColor(int index) {
+        int i =0;
+        for(Ring ring:rings) {
+            if(i == index) {
+                return ring.getColor();
+            }
+            i++;
+        }
+        return 0;
     }
     public void pause() {
         isRunning = false;
