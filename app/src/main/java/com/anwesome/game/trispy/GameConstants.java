ce8b60320160c5aa679ce7ce245ab20056eeb0f2
Anwesh43 <anweshthecool0@gmail.com>
2017-01-22 09:04:13 +0530

    Adding constants file

---
+++ app/src/main/java/com/anwesome/game/trispy/GameConstants.java
@@ -0,0 +1,15 @@
+package com.anwesome.game.trispy;
+
+import android.graphics.Color;
+
+/**
+ * Created by anweshmishra on 22/01/17.
+ */
+public class GameConstants {
+    public static final int GAME_DELAY = 100;
+    public static final int ROTATING_LINE_COLOR = Color.parseColor("#EEEEEE");
+    public static final int RING_RADIUS_SCALE = 25;
+    public static final int STROKE_SIZE = 25;
+    public static final int LINE_SCALE = 10;
+    public static final int[] colors = {Color.parseColor("#009688"),Color.parseColor("#2196F3"),Color.parseColor("#FF5722"),Color.parseColor("#e53935"),Color.parseColor("#4CAF50"),Color.parseColor("#9C27B0")};
+}
