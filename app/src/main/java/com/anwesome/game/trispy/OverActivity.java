4f444df6b8fc8f0698f0e8da5ebc7b775a7a3255
Anwesh43 <anweshthecool0@gmail.com>
2017-01-23 15:51:34 +0530

    Adding Over activity and game activity

---
+++ app/src/main/java/com/anwesome/game/trispy/OverActivity.java
@@ -0,0 +1,15 @@
+package com.anwesome.game.trispy;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+
+/**
+ * Created by anweshmishra on 23/01/17.
+ */
+public class OverActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
