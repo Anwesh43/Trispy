ae72826f1365d43a2f089b07cfeb694e39e10f73
Anwesh43 <anweshthecool0@gmail.com>
2017-01-28 03:15:24 +0530

    Pressing back to exit

--- app/src/main/java/com/anwesome/game/trispy/MainActivity.java
+++ app/src/main/java/com/anwesome/game/trispy/MainActivity.java
@@ -11,12 +11,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Display;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.anwesome.game.trispy.gameobjects.MenuBall;
 import com.anwesome.game.trispy.views.GameView;
 import com.anwesome.game.trispy.views.InteractiveView;
 
 public class MainActivity extends InteractiveActivity {
+    private int backCount = 0;
     public void createMenus(InteractiveView interactiveView) {
         MenuBall playBall = MenuBall.newInstance(BitmapFactory.decodeResource(getResources(),R.drawable.play),12, Color.parseColor("#b71c1c"));
         playBall.setX(5*w/6);
@@ -29,5 +31,15 @@ public class MainActivity extends InteractiveActivity {
             }
         });
         interactiveView.setMenuBalls(playBall);
+    }
+    public void onBackPressed() {
+        if(backCount == 0) {
+            Toast.makeText(this,"Press Back Button Again To Exit",Toast.LENGTH_SHORT).show();
+        }
+        else {
+            finishAffinity();
+            System.exit(0);
+        }
+        backCount++;
     }
 }
