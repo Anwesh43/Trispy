d9fafeec1a6fe56849e741fc4144a218d26ef206
Anwesh43 <anweshthecool0@gmail.com>
2017-02-27 17:03:21 +0530

    Instruction mode store to store the mode of instruction

--- app/src/main/java/com/anwesome/game/trispy/utils/InstructionModeStore.java
+++ app/src/main/java/com/anwesome/game/trispy/utils/InstructionModeStore.java
@@ -4,4 +4,11 @@ package com.anwesome.game.trispy.utils;
  * Created by anweshmishra on 27/02/17.
  */
 public class InstructionModeStore {
+    private int mode = 0;
+    public void setMode(int mode) {
+        this.mode = mode;
+    }
+    public int getMode() {
+        return mode;
+    }
 }
