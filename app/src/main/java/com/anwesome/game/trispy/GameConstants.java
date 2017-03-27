33b48628ce6f1a6460fc3a1c38cb54547a51100f
Anwesh43 <anweshthecool0@gmail.com>
2017-03-27 13:47:32 +0530

    decreasing stroke size

--- app/src/main/java/com/anwesome/game/trispy/GameConstants.java
+++ app/src/main/java/com/anwesome/game/trispy/GameConstants.java
@@ -6,12 +6,12 @@ import android.graphics.Color;
  * Created by anweshmishra on 22/01/17.
  */
 public class GameConstants {
-    public static final int GAME_DELAY = 42;
+    public static final int GAME_DELAY = 30;
     public static final int ROTATING_LINE_COLOR = Color.parseColor("#EEEEEE");
     public static final int RING_RADIUS_SCALE = 40;
     public static final float ROTATING_SPEED = 6;
     public static final int BACK_COLOR = Color.parseColor("#00ACC1");
-    public static final int STROKE_SIZE = 20;
+    public static final int STROKE_SIZE = 10;
     public static final float LINE_SCALE = 2.5f;
     public static final String GAME_OVER_TEXT ="GAME OVER";
     public static final String SCORE_PREF="SCORE_PREF";
